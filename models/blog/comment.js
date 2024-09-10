7bbd14c4a617cc4a3c5a94c116adeb7903ef8143
katmafalela <108935975+katmafalela@users.noreply.github.com>
2024-09-10 10:14:58 +0200

    Added the blog comments schema

---
+++ models/blog/comment.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    trim: true 
+  },
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true 
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog', 
+    required: true
+  },
+  parentComment: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Comment', 
+    default: null 
+  },
+  isApproved: { 
+    type: Boolean,
+    default: true 
+  },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Comment', commentSchema);
