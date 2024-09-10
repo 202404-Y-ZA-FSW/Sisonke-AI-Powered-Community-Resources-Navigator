c79e5cdaa826d2f9d41d65a88157ff7998a57567
katmafalela <108935975+katmafalela@users.noreply.github.com>
2024-09-10 12:55:13 +0200

    Update comment.js

--- models/blog/comment.js
+++ models/blog/comment.js
@@ -16,15 +16,6 @@ const commentSchema = new mongoose.Schema({
     ref: 'Blog', 
     required: true
   },
-  parentComment: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Comment', 
-    default: null 
-  },
-  isApproved: { 
-    type: Boolean,
-    default: true 
-  },
-}, { timestamps: true });
+  { timestamps: true });
 
 module.exports = mongoose.model('Comment', commentSchema);
