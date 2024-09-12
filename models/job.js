1c99d6a0bdbc6d1f44c5163ef97f3e31fef0c9df
MissAngelaKing <145587610+MissAngelaKing@users.noreply.github.com>
2024-09-12 19:57:03 +0200

    job-listings-model-changes

--- models/job.js
+++ models/job.js
@@ -1,76 +1,89 @@
 const mongoose = require('mongoose');
 
+// Defining the schema for job listings
 const jobListingsSchema = new mongoose.Schema({
+  // Job title
   title: {
     type: String,
     required: true,
   },
+  // Company name
   company: {
     type: String,
     required: true,
   },
+  // Location of the job
   location: {
     type: String,
     required: true,
   },
+  // Type of job 
   jobType: {
     type: String,
-    enum: ['full-time', 'part-time', 'contract', 'freelance', 'internship'],
+    enum: ['full-time', 'part-time', 'contract', 'freelance', 'internship', 'learnership'],
     required: true,
   },
+  // Salary for the job
   salary: {
     type: Number,
   },
+  // Description of the job
   description: {
     type: String,
     required: true,
   },
+  // Date when the job was posted
   datePosted: {
     type: Date,
     default: Date.now,
   },
+  // Qualifications required for the job
   qualifications: {
     type: [String],
   },
+  // Experience level required
   experienceLevel: {
     type: String,
     enum: ['entry-level', 'mid-level', 'senior-level'],
   },
+  // Education requirements for the job
   education: {
     type: [String],
   },
-  companySize: {
-    type: String,
-    enum: ['small', 'medium', 'large'],
-  },
+  // Required skills for the job
   requiredSkills: {
     type: [String],
   },
-  applicationStatus: {
+  // Link to the job application page or further details
+  link: {
     type: String,
-    enum: ['applied', 'in review', 'interview', 'offered', 'rejected'],
-    default: 'applied',
+    validate: {
+      validator: function(v) {
+        // Simple URL validation
+        return /^(ftp|http|https):\/\/[^ "]+$/.test(v);
+      },
+      message: 'Invalid URL'
+    },
   },
+  // Users who are matched with this job
   matchedUsers: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     index: true,
   }],
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    index: true,
-  },
+  // Users who have saved this job listing
   savedBy: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     index: true,
   }],
 }, {
+  // Include timestamps for creation and last update
   timestamps: true,
 });
 
+// Create the JobListings model using the schema
 const JobListings = mongoose.model('JobListings', jobListingsSchema);
+
 
 module.exports = JobListings;
