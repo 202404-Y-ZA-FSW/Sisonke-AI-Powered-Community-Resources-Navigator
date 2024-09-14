a84773272b14d9c96c4a8e6839484591e340fc5a
Geraldmutsw <geraldmutswi@gmail.com>
2024-09-14 14:38:21 +0200

    Resolved blog likes issues, controllers and routes

--- backend/routes/blog/like.js
+++ backend/routes/blog/like.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
+// LIKE CONTROLLER
 const likeController = require('../../controllers/blog/like');
 
-router.post('/like',  likeController.likeBlog); 
+// MIDDLIEWARE
+const authenticationMiddleware = require('../../middleware/isAuthenticated');
+
+router.post('/like',  likeController.newLike); 
+router.get('/likes/:blogID', likeController.getAllLikedBlogs);
+router.delete('/unlike/:blogID', likeController.unlike);
+router.get('/like/count/:blogID', likeController.getLikeCount);
 
 module.exports = router;
