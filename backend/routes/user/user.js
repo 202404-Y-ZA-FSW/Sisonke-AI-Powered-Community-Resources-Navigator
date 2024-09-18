e70e73c7cd8439162cf12441e6b2a6622d567286
katmafalela <108935975+katmafalela@users.noreply.github.com>
2024-09-18 17:17:20 +0200

    Added the file structure for Hophney

---
+++ backend/routes/user/user.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../controllers/user/userProfile');
+
+router.post('/create', User.createUser);
+router.put('/update/:id', User.updateUser); 
+
+module.exports = router; 
