8429723db1bb9b6527cce72d018d7468b126c5d6
MissAngelaKing <145587610+MissAngelaKing@users.noreply.github.com>
2024-09-23 11:37:53 +0200

    register

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,12 @@
 "use client"
 import LoginPage from "../app/account/login/page";
+import Register from "../app/account/register/page";
 
 export default function Home() {
   return (
     <>
     <LoginPage />
+    <Register />
     </>
   );
 }
