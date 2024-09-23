cd0fcb712cf3b4aceaca26285fe200f759a5f1b3
Ntandoyenkosi Ntshangase <khulile.ntshangase@gmail.com>
2024-09-23 12:13:26 +0200

    added footer to homepage

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
 "use client"
-import LoginPage from "../app/account/login/page";
-import Register from "../app/account/register/page";
-
-export default function Home() {
-  return (
-    <>
-    <LoginPage />
-    <Register />
-    </>
-=======
 import React from "react";
 import Navbar from "./components/sections/NavBar";
 import Hero from "./components/sections/Hero";
+import Footer from "./components/sections/Footer";
 export default function Home() {
   return (
     <React.Fragment>
       <Navbar/>
       <Hero/>
+      <Footer/>
     </React.Fragment>
->>>>>>> 3ad839b2970a59f819bddb0ca1dab3fb9189ac8d
   );
 }
