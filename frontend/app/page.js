99cf517f1641a078abb602264ceb5a89e68c1b66
Geraldmutsw <geraldmutswi@gmail.com>
2024-09-26 10:12:21 +0200

    Completed the subscriber section

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -4,12 +4,14 @@ import Navbar from "./components/sections/NavBar";
 import Hero from "./components/sections/Hero";
 import Footer from "./components/sections/Footer";
 import Jobs from "./components/sections/Jobs";
+import Subscriber from "./components/sections/Subscribe";
 export default function Home() {
   return (
     <React.Fragment>
       <Navbar/>
       <Hero/>
       <Jobs/>
+      <Subscriber/>
       <Footer/>
     </React.Fragment>
   );
