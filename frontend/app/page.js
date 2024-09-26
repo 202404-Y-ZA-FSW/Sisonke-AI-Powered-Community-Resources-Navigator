00cba4a2eaadce0a94a0936f35cbafe59526ab7b
Geraldmutsw <geraldmutswi@gmail.com>
2024-09-26 10:23:32 +0200

    Fixed a bug on the navbar

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -5,12 +5,14 @@ import Hero from "./components/sections/Hero";
 import Footer from "./components/sections/Footer";
 import Jobs from "./components/sections/Jobs";
 import Subscriber from "./components/sections/Subscribe";
+import FAQs from "./components/sections/FAQs";
 export default function Home() {
   return (
     <React.Fragment>
       <Navbar/>
       <Hero/>
       <Jobs/>
+      <FAQs/>
       <Subscriber/>
       <Footer/>
     </React.Fragment>
