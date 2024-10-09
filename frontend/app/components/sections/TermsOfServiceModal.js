786e248f2b6659a5eeb5c70884c25a7e53888ccb
Hophney <131249003+Hophneylen@users.noreply.github.com>
2024-10-09 18:41:01 +0200

    Update TermsOfServiceModal.js

--- frontend/app/components/sections/TermsOfServiceModal.js
+++ frontend/app/components/sections/TermsOfServiceModal.js
@@ -1,5 +1,3 @@
-// components/TermsOfServiceModal.js
-
 import React from 'react';
 import { Modal, Box, Typography, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
